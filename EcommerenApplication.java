125bdc45a3f98012f99cd60cd3ea1fffc3e83d72
DuongNN194034 <laserbean4@gmail.com>
2022-11-04 22:50:35 +0700

    Initial commit

---
+++ EcommerenApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommeren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerenApplication.class, args);
+	}
+
+}
